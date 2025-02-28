10194aac12a02c3e2ee4cb6b7766d115bda4d8a4
Safa <safashujath@gmail.com>
2025-02-28 08:37:39 -0600

    edits

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,24 +1,31 @@
 import Navbar from "../components/NavBar"
 import styles from "./LandingPage.module.css"
 import panda from "../assets/panda.png"
+import Projects from "./Projects"
 
 function LandingPage() {
     return (
         <div>
             <div className={styles['landing-container']}>
+                <Navbar />
                 <div className={styles['main-container']}>
                     <div className={styles['landing-left']}>
-                       
+                        <h2>hello, i'm</h2>
+                        <h2 className={styles['main-heading']}>safa</h2>
+                        <h2 className={styles['typewriter']}>shujath</h2>
+
+
                     </div>
 
                     <div className={styles['landing-right']}>
                         <img src={panda} alt="Logo" width={400} />
                     </div>
                 </div>
-                
+
             </div>
+            <Projects />
         </div>
-        
+
     )
 }
 
